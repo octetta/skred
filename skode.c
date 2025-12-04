8bc38e7b45a931ea04dc4e0ce39229439b03141a
octetta <joseph.stewart@gmail.com>
2025-12-04 23:34:17 +0100

    Hide multichar constant warnings Move demo related items into ifdef Remove unused states Add enum for fn modes Hide internal things Move vars to typedef Name things better

--- skode.c
+++ skode.c
@@ -4,10 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "linenoise.h"
-
-#define HISTORY_FILE ".skode_history"
-
 #define IS_NUMBER(c) (isdigit(c) || strchr("-.", c))
 #define IS_SEPARATOR(c) (isspace(c) || c == ',')
 #define IS_STRING(c) (c == '{')
@@ -35,13 +31,12 @@ enum {
   GET_ARRAY,
   GET_COMMENT,
   CHUNK_END,
-  DECIDE,
-  ERROR,
+  //
+  FUNCTION,
+  DEFER,
 };
 
-#define GET_STRING_MAX (1024)
-
-double skode_strtod(char *s) {
+static double skode_strtod(char *s) {
   double d = NAN;
   if (s[1] == '\0' && (s[0] == '-' || s[0] == 'e' || s[0] == '.')) return d;
   d = strtod(s, NULL);
@@ -52,6 +47,7 @@ double skode_strtod(char *s) {
 #define NUM_ACC_MAX (1024)
 #define ATOM_MAX (4)
 #define ATOM_NIL (0x5f5f5f5f)
+#define VAR_MAX (26)
 
 typedef struct skode_s {
   // scratch string
@@ -83,25 +79,30 @@ typedef struct skode_s {
   int atom_num;
   //
   int state;
+  //
+  double global_var[VAR_MAX];
+  double local_var[VAR_MAX];
+  //
+  int (*fn)(struct skode_s *s, int info);
   void *user;
 } skode_t;
 
-skode_t *skode_new(void *user) {
+skode_t *skode_new(int (*fn)(skode_t *s, int info), void *user) {
   skode_t *s = (skode_t*)malloc(sizeof(skode_t));
   s->scr_cap = 1024;
   s->scr_len = 0;
-  s->scr_acc = (char *)malloc(s->scr_cap * sizeof(char));
+  s->scr_acc = (char*)malloc(s->scr_cap * sizeof(char));
   //
   s->num_cap = 1024;
   s->num_len = 0;
   //
   s->data_cap = 1024;
   s->data_len = 0;
-  s->data = (double *)malloc(s->data_cap * sizeof(double));
+  s->data = (double*)malloc(s->data_cap * sizeof(double));
   //
   s->defer_cap = 1024;
   s->defer_len = 0;
-  s->defer_acc = (char *)malloc(s->defer_cap * sizeof(char));
+  s->defer_acc = (char*)malloc(s->defer_cap * sizeof(char));
   s->defer_num = 0;
   s->defer_mode = '?';
   //
@@ -112,7 +113,10 @@ skode_t *skode_new(void *user) {
   s->atom_len = 0;
   s->atom_num = ATOM_NIL;
   //
+  s->fn = fn;
   s->user = user;
+  //
+  s->state = START;
   return s;
 }
 
@@ -133,11 +137,9 @@ void skode_free(skode_t *s) {
   s->defer_len = 0;
 }
 
-//
-
 void string_clear(skode_t *s) {
   s->scr_len = 0;
-  s->scr_acc[s->scr_len] = '\0';
+  s->scr_acc[0] = '\0';
 }
 
 void string_push(skode_t *s, char c) {
@@ -149,7 +151,7 @@ void string_push(skode_t *s, char c) {
 
 void num_clear(skode_t *s) {
   s->num_len = 0;
-  s->num_acc[s->num_len] = '\0';
+  s->num_acc[0] = '\0';
 }
 
 void num_push(skode_t *s, char c) {
@@ -172,12 +174,17 @@ void array_push(skode_t *s) {
   num_clear(s);
 }
 
-void arg_clear(skode_t *s)  { s->arg_len = 0; }
-void arg_push(skode_t *s, double d) { if (s->arg_len < s->arg_cap) s->arg[s->arg_len++] = d; }
+void arg_clear(skode_t *s)  {
+  s->arg_len = 0;
+}
+
+void arg_push(skode_t *s, double d) {
+  if (s->arg_len < s->arg_cap) s->arg[s->arg_len++] = d;
+}
 
 void defer_clear(skode_t *s) {
   s->defer_len = 0;
-  s->defer_acc[s->defer_len] = '\0';
+  s->defer_acc[0] = '\0';
 }
 
 void defer_push(skode_t *s, char c) {
@@ -214,23 +221,18 @@ void atom_reset(skode_t *s) {
 }
 
 char *atom_string(int i) {
-  static char s[5] = "\0\0\0\0\0";
+  static char s[5] = "****";
   char *p = (char *)&i;
-  for (int i=0; i<4; i++) s[i] = p[i];
+  for (int i=0; i<4; i++) s[3-i] = p[i];
   return s;
 }
 
-int decide(skode_t *s, int state, int (*fn)(skode_t *s)) {
-  double d;
-  float f;
-  int i;
-  unsigned int u;
-  char *p;
+static int action(skode_t *s, int state) {
   switch (state) {
     case CHUNK_END:
     case GET_ATOM:
       if (s->atom_num != ATOM_NIL) {
-        fn(s);
+        s->fn(s, FUNCTION);
         atom_reset(s);
         arg_clear(s);
       }
@@ -240,72 +242,55 @@ int decide(skode_t *s, int state, int (*fn)(skode_t *s)) {
     case GET_NUMBER:
       arg_push(s, num_get(s));
       num_clear(s);
-      return START;
       break;
     case GET_DEFER_STRING:
-      printf("DEFER %c %g '%s'\n", s->defer_mode, s->defer_num, s->defer_acc);
+      s->fn(s, DEFER);
       defer_clear(s);
       break;
   }
   return START;
 }
 
-#define VAR_MAX (26)
-double local_var[VAR_MAX];
-double global_var[VAR_MAX];
-
-int parse(skode_t *s, char *line, int *entry_state, int (*fn)(skode_t *s)) {
-  static int first = 1;
-  if (first) {
-    for (int i=0; i<VAR_MAX; i++) {
-      local_var[i] = i+1000;
-      global_var[i] = i+2000;
-    }
-    first = 0;
-  }
-  int state = *entry_state;
+int parse(skode_t *s, char *line, int (*go)(skode_t *s, int info)) {
   char *ptr = line;
   char *end = ptr + strlen(ptr);
-  char *tmp;
-  char next;
   while (1) {
     if (ptr >= end) {
-      switch (state) {
+      switch (s->state) {
         case GET_ATOM:
-          decide(s, state, fn);
-          state = START;
+          action(s, s->state);
+          s->state = START;
           break;
         case GET_NUMBER:
-          decide(s, state, fn);
-          state = START;
+          action(s, s->state);
+          s->state = START;
           break;
         default:
           break;
       }
       break;
     }
-    next = *(ptr+1);
     reprocess:
-    switch (state) {
+    switch (s->state) {
       case START:
         if (IS_NUMBER(*ptr)) {
           num_clear(s);
           num_push(s, *ptr);
-          state = GET_NUMBER;
+          s->state = GET_NUMBER;
         }
         else if (IS_SEPARATOR(*ptr)) { /* skip whitespace and , */ }
-        else if (IS_STRING(*ptr))    { string_clear(s); state = GET_STRING; }
-        else if (IS_ARRAY(*ptr))     { num_clear(s); array_clear(s); state = GET_ARRAY; }
-        else if (IS_VARIABLE(*ptr))  { state = GET_VARIABLE; }
-        else if (IS_COMMENT(*ptr))   { state = GET_COMMENT; }
-        else if (IS_CHUNK_END(*ptr)) { decide(s, CHUNK_END, fn); state = START; }
-        else if (IS_DEFER(*ptr))     { decide(s, CHUNK_END, fn); s->defer_mode = *ptr; state = GET_DEFER_NUMBER; }
+        else if (IS_STRING(*ptr))    { string_clear(s); s->state = GET_STRING; }
+        else if (IS_ARRAY(*ptr))     { num_clear(s); array_clear(s); s->state = GET_ARRAY; }
+        else if (IS_VARIABLE(*ptr))  { s->state = GET_VARIABLE; }
+        else if (IS_COMMENT(*ptr))   { s->state = GET_COMMENT; }
+        else if (IS_CHUNK_END(*ptr)) { action(s, CHUNK_END); s->state = START; }
+        else if (IS_DEFER(*ptr))     { action(s, CHUNK_END); s->defer_mode = *ptr; s->state = GET_DEFER_NUMBER; }
         else if (iscntrl(*ptr)) { puts("# iscntrl !!!!"); }
         else {
           // i hope this is at the right catch point...
           atom_clear(s);
           atom_push(s, *ptr);
-          state = GET_ATOM;
+          s->state = GET_ATOM;
         }
         break;
       case GET_NUMBER:
@@ -314,15 +299,15 @@ int parse(skode_t *s, char *line, int *entry_state, int (*fn)(skode_t *s)) {
         } else if (*ptr == '$') {
           printf("VAR?");
         } else {
-          state = decide(s, state, fn);
+          s->state = action(s, s->state);
           // we got a character we need to process
           goto reprocess;
         }
         break;
       case GET_STRING:
         if (IS_STRING_END(*ptr)) {
-          decide(s, state, fn);
-          state = START;
+          action(s, s->state);
+          s->state = START;
         } else {
           string_push(s, *ptr);
         }
@@ -330,8 +315,8 @@ int parse(skode_t *s, char *line, int *entry_state, int (*fn)(skode_t *s)) {
       case GET_ARRAY:
         if (IS_ARRAY_END(*ptr)) {
           array_push(s);
-          decide(s, state, fn);
-          state = START;
+          action(s, s->state);
+          s->state = START;
         } else if (IS_NUMBER_EX(*ptr)) {
           num_push(s, *ptr);
         } else if (IS_SEPARATOR(*ptr)) {
@@ -343,11 +328,11 @@ int parse(skode_t *s, char *line, int *entry_state, int (*fn)(skode_t *s)) {
         break;
       case GET_COMMENT:
         if (IS_CHUNK_END(*ptr)) {
-          decide(s, CHUNK_END, fn);
-          state = START;
+          action(s, CHUNK_END);
+          s->state = START;
         } else if (*ptr == '\n') {
-          decide(s, state, fn);
-          state = START;
+          action(s, s->state);
+          s->state = START;
         }
         break;
       case GET_VARIABLE:
@@ -357,19 +342,19 @@ int parse(skode_t *s, char *line, int *entry_state, int (*fn)(skode_t *s)) {
           if (islower(*ptr)) {
             // a-z
             i = (*ptr) - 'a';
-            d = local_var[i];
+            d = s->local_var[i];
           } else {
             // A-Z
             i = (*ptr) - 'A';
-            d = global_var[i];
+            d = s->global_var[i];
           }
           arg_push(s, d);
         } else {
           // not a var, so ignore and hope the next this is valid
-          state = START;
+          s->state = START;
           goto reprocess;
         }
-        state = START;
+        s->state = START;
         break;
       case GET_DEFER_NUMBER:
         if (IS_NUMBER(*ptr)) {
@@ -377,18 +362,18 @@ int parse(skode_t *s, char *line, int *entry_state, int (*fn)(skode_t *s)) {
         } else {
           s->defer_num = num_get(s);
           num_clear(s);
-          state = GET_DEFER_STRING;
+          s->state = GET_DEFER_STRING;
           goto reprocess;
         }
         break;
       case GET_DEFER_STRING:
         if (IS_DEFER(*ptr)) {
           s->defer_mode = *ptr;
-          decide(s, GET_DEFER_STRING, fn);
-          state = GET_DEFER_NUMBER;
+          action(s, GET_DEFER_STRING);
+          s->state = GET_DEFER_NUMBER;
         } else if (IS_CHUNK_END(*ptr)) {
-          decide(s, GET_DEFER_STRING, fn);
-          state = START;
+          action(s, GET_DEFER_STRING);
+          s->state = START;
         } else {
           defer_push(s, *ptr);
         }
@@ -397,58 +382,72 @@ int parse(skode_t *s, char *line, int *entry_state, int (*fn)(skode_t *s)) {
         if (IS_ATOM(*ptr)) {
           atom_push(s, *ptr);
         } else {
-          decide(s, state, fn);
-          state = START;
+          action(s, s->state);
+          s->state = START;
           goto reprocess;
         }
         break;
       default:
         puts("default ->START");
-        decide(s, state, fn);
-        state = START;
+        action(s, s->state);
+        s->state = START;
         break;
     } 
     ptr++;
   }
-  *entry_state = state;
+  return 0;
 }
 
 #ifdef DEMO
-int demo(skode_t *s) {
+#include "linenoise.h"
+#define HISTORY_FILE ".skode_history"
+int demo(skode_t *s, int info) {
   int *user = (int*)s->user;
-  printf("DO %s WITH [", atom_string(s->atom_num));
-  // run the callback here
-  if (s->arg_len) {
-    for (int n=0; n<s->arg_len; n++) printf(" %g", s->arg[n]);
+  if (info == FUNCTION) {
+    printf("FUNCTION %s [", atom_string(s->atom_num));
+    // run the callback here
+    if (s->arg_len) {
+      for (int n=0; n<s->arg_len; n++) printf(" %g", s->arg[n]);
+    }
+    printf(" ]");
+    if (s->scr_len) printf(" {%s}", s->scr_acc);
+    if (s->data_len) {
+      printf(" (");
+      for (int i=0; i<s->data_len; i++) {
+        if (i < 4 || i == s->data_len-1) printf(" %g", s->data[i]);
+        else if (i == 4) printf(" ...skip %d...", s->data_len-5);
+      }
+      printf(" )");
+    }
+    printf("\n");
+    switch (s->atom_num) {
+      case '=a__':
+        s->local_var[0] = s->arg[0];
+        break;
+      case ':q__':
+        printf("QUIT\n");
+        *user = -1;
+        break;
+    }
+  } else if (info == DEFER) {
+    printf("DEFER %c %g '%s'\n", s->defer_mode, s->defer_num, s->defer_acc);
   }
-  printf(" ]\n");
-  if (s->scr_len) printf("{%s}\n", s->scr_acc);
-  if (s->data_len) {
-    printf("(");
-    for (int i=0; i<s->data_len; i++) printf(" %g", s->data[i]);
-    printf(" )\n");
-  }
-  switch (s->atom_num) {
-    case '=a__':
-      local_var[0] = s->arg[0];
-      break;
-    case ':q__':
-      printf("QUIT\n");
-      *user = -1;
-      break;
-  }
+  return 0;
 }
 int main(int argc, char *arg[]) {
   int user = 0;
-  skode_t *s = skode_new(&user);
+  skode_t *s = skode_new(demo, &user);
+  for (int i=0; i<VAR_MAX; i++) {
+    s->local_var[i] = i+1000;
+    s->global_var[i] = i+2000;
+  }
   linenoiseHistoryLoad(HISTORY_FILE);
-  int state = START;
   while (1) {
     char *line = NULL;
     line = linenoise("# ");
     if (line == NULL) break;
     linenoiseHistoryAdd(line);
-    int r = parse(s, line, &state, demo);
+    parse(s, line, demo);
     linenoiseFree(line);
     if (user == -1) {
       printf("must quit\n");
