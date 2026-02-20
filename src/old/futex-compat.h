33f7299297ff90fcfe0b93197a8f1ea3a7a12150
Joseph Stewart <joseph.stewart@gmail.com>
2026-02-20 10:33:51 +0100

    More clean-up of old stuff

--- src/old/futex-compat.h
+++
@@ -1,8 +0,0 @@
-// futex-compat.h - Cross-platform futex-like implementation
-#ifndef FUTEX_COMPAT_H
-#define FUTEX_COMPAT_H
-
-inline long futex_wait_timeout(volatile uint32_t *uaddr, uint32_t expected, int timeout_ms);
-inline long futex_wake(volatile uint32_t *uaddr, int num_wake);
-
-#endif // FUTEX_COMPAT_H
